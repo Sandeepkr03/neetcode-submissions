0ec62027b4b344fc53cf73943a6e38e4bdf424b2
Sandeep Kumar <114747614+Sandeepkr03@users.noreply.github.com>
2026-06-17 16:41:54 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        dict1 = {}
+        for char in s:
+            dict1[char] = dict1.get(char,0)+1
+        dict2 = {}
+        for char in t:
+            dict2[char] = dict2.get(char,0)+1
+            
+        return dict1==dict2        
